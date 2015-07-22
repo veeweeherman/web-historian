6a0ce36b35085d427ac30eab61cd2d0d326d5617
veeweeherman <vy.hoang.cu@gmail.com>
2015-07-22 12:24:21 -0700

    got server to run on localhost

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
+var url = require('url');
 
 exports.handleRequest = function (req, res) {
   res.end(archive.paths.list);
