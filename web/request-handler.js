085065e947c21c449944f4d99abf10ffcd4b7d73
TGA-Student <tga.student@hackreactor.com>
2015-07-22 19:34:16 -0700

    Passed 1st test

--- web/request-handler.js
+++ web/request-handler.js
@@ -2,6 +2,9 @@ var path = require('path');
 var archive = require('../helpers/archive-helpers');
 var indexPage = require('../archives/sites.txt');
 var url = require('url');
+var helpers = require('./http-helpers.js');
+var destination = require('./archives/sites.txt');
+
 var messages = { // Object to store results
   results: []
 };
@@ -10,24 +13,16 @@ var statusCode = null;
 exports.handleRequest = function (req, res) {
   if(req.method === 'GET'){
     statusCode = 200;
-    message = JSON.stringify(indexPage);
-    reponse.writeHead("THIS IS YOUR ARCHIVE SPARTAAAA")
     
-  } else if (req.method === 'POST'){
-      var data = ''; 
-      request.on('data', function (chunk) { // in case data comes in chunks, we put the pieces together in data
-        data += chunk.toString();
-      });
-      request.on('end', function (){ // event fires when data is complete
-        messages.results.push(JSON.parse(data)); // stores message in messages object
-        
-      })
-    console.log('this is the POST message');
-  } else if(req.method === 'OPTIONS'){
-    // console.log('this is the OPTIONS message');
-  }
+    archive.paths.list = './archives/sites.txt';
+    res.writeHead(statusCode, headers);
+    // readListOfURLs();
+    
+  } 
 
-  res.end("wooooowww OH HAY GURL HAYYYY",archive.paths.list);
+  res.end(archive.paths.list);
 };
 
 
+// pass test 1: write out the GET
+// make res.end be the thing to return (contents of index.html)
