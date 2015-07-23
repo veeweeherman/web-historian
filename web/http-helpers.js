b9c953d1151959e0faec4e7258019ebc57ef5cfc
Kim Merino <dafabulousteach@gmail.com>
2015-07-22 21:34:41 -0700

    Files from local machine

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -15,7 +15,4 @@ exports.serveAssets = function(res, asset, callback) {
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
 };
-
-
-
-// As you progress, keep thinking about what helper functions you can put here!
+ 
